d3167d302fd36d618f3b4747b0b5ddf2184ef13f
faisalalmail <faisalyounis90@gmail.com>
2025-01-19 10:30:13 +0300

    project finish

--- circleshape.py
+++ circleshape.py
@@ -20,3 +20,9 @@ class CircleShape(pygame.sprite.Sprite):
     def update(self, dt):
         # sub-classes must override
         pass
+
+    def collides_with(self, other):
+        return self.position.distance_to(other.position) <= self.radius + other.radius
+    
+    def bullet_collide(self, other):
+        return self.position.distance_to(other.position) <= self.radius + other.radius
